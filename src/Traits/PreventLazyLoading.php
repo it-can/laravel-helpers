dfb7ad2342e052f02f30bfb7ad251adc84fd2eb3
Michiel Vugteveen <info@it-can.nl>
2019-06-11 15:15:01 +0200

    add trait

---
+++ src/Traits/PreventLazyLoading.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ITCAN\LaravelHelpers\Traits;
+
+trait PreventLazyLoading
+{
+    /**
+     * Get a relationship value from a method.
+     *
+     * @param  string  $method
+     * @return mixed
+     *
+     * @throws \LogicException
+     */
+    protected function getRelationshipFromMethod($method)
+    {
+        $whitelist = $this->allowedLazyRelations ?? [];
+
+        if ($this->exists and ! in_array($method, $whitelist))
+        {
+            $model = get_class($this);
+
+            throw new Exception("Tried to load {$method} in {$model}");
+        }
+
+        return parent::getRelationshipFromMethod($method);
+    }
+}
