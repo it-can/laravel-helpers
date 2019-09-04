ccc4bf3c849419447822685fffb120ab022f92da
Michiel Vugteveen <info@it-can.nl>
2019-09-04 13:39:52 +0200

    remove unused

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,24 +3,17 @@
 namespace ITCAN\LaravelHelpers\Tests;
 
 use Mockery;
-use Illuminated\Testing\TestingTools;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
 Mockery::globalHelpers();
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
-    use TestingTools;
     use MockeryPHPUnitIntegration;
     public static $functions;
 
     protected function setUp()
     {
         self::$functions = mock();
-    }
-
-    protected function expectsExecWith($command)
-    {
-        self::$functions->expects()->exec($command);
     }
 }
