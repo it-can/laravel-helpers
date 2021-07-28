02a5d83ad8a4c675b7e8d373af91a9de97fea71e
Michiel Vugteveen <info@it-can.nl>
2021-07-28 12:29:29 +0200

    add toRawSql() macro

--- src/GlobalHelpersServiceProvider.php
+++ src/GlobalHelpersServiceProvider.php
@@ -2,11 +2,14 @@
 
 namespace ITCAN\LaravelHelpers;
 
+use DateTime;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 
 class GlobalHelpersServiceProvider extends ServiceProvider
 {
@@ -49,25 +52,62 @@ class GlobalHelpersServiceProvider extends ServiceProvider
             return 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
         });
 
-        if (! Response::hasMacro('pdf')) {
-            Response::macro('pdf', function ($content, $filename, $return_string = false) {
-                if ($return_string) {
-                    return $content;
-                }
+        /*
+         * Allow to return PDF files in browser instead of download
+         */
+        Response::macro('pdf', function ($content, $filename, $return_string = false) {
+            if ($return_string) {
+                return $content;
+            }
 
-                $headers = [
-                    'Content-Type'        => 'application/pdf',
-                    'Content-Disposition' => 'inline; filename="' . $filename . '"',
-                ];
+            $headers = [
+                'Content-Type'        => 'application/pdf',
+                'Content-Disposition' => 'inline; filename="' . $filename . '"',
+            ];
 
-                return Response::make($content, 200, $headers);
-            });
-        }
+            return Response::make($content, 200, $headers);
+        });
 
+        /**
+         * Add dot function to Collection
+         */
         Collection::macro('dot', function ($key, $default = null) {
             $value = Arr::get($this, $key, $default);
 
             return is_iterable($value) ? collect($value) : $value;
         });
+
+        // Original idea by @therobfonz
+        // https://twitter.com/kirschbaum_dev/status/1418590965368074241
+        // Refined by BinaryKitten
+        // https://gist.github.com/BinaryKitten/2873e11daf3c0130b5a19f6b94315033
+        QueryBuilder::macro(
+            'toRawSql',
+            function () {
+                return array_reduce(
+                    $this->getBindings(),
+                    static function ($sql, $binding) {
+                        if ($binding instanceof DateTime) {
+                            $binding = $binding->format('Y-m-d H:i:s');
+                        }
+
+                        return preg_replace(
+                            '/\?/',
+                            is_string($binding) ? "'" . $binding . "'" : $binding,
+                            $sql,
+                            1
+                        );
+                    },
+                    $this->toSql()
+                );
+            }
+        );
+
+        EloquentBuilder::macro(
+            'toRawSql',
+            function () {
+                return $this->getQuery()->toRawSql();
+            }
+        );
     }
 }
