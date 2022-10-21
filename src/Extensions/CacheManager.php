b0c9d4b1aaf40b345178746e87831058e6ad192c
Michiel Vugteveen <info@it-can.nl>
2022-10-21 11:28:27 +0200

    fix

--- src/Extensions/CacheManager.php
+++ src/Extensions/CacheManager.php
@@ -9,18 +9,20 @@ class CacheManager extends BaseCacheManager
     /**
      * Create an instance of the Redis cache driver.
      *
-     * @param  array  $config
+     * @param array $config
      * @return \Illuminate\Cache\Repository
      */
     protected function createRedisDriver(array $config)
     {
         $redisConfig = config('database.redis', []);
+        $connection = $config['connection'] ?? 'default';
 
         // Load custom RedisStore when using phpredis (this will take care of serialization by the extension)
-        if (isset($redisConfig['client']) && $redisConfig['client'] === 'phpredis') {
+        if (isset($redisConfig['client'])
+            && $redisConfig['client'] === 'phpredis'
+            && (isset($redisConfig['options']['serializer']) || isset($redisConfig[$connection]['options']['serializer']))
+        ) {
             $redis = $this->app['redis'];
-
-            $connection = $config['connection'] ?? 'default';
 
             $store = new RedisStore($redis, $this->getPrefix($config), $connection);
 
