cf96089d16d6f8268be7e3a15bbab911c1f32769
Michiel Vugteveen <info@it-can.nl>
2023-05-30 14:35:03 +0200

    allow adding extra headers

--- src/Response/PdfResponse.php
+++ src/Response/PdfResponse.php
@@ -6,12 +6,16 @@ use Illuminate\Http\Response;
 
 class PdfResponse extends Response
 {
-    public function __construct($content, $filename)
+    public function __construct($content, $filename, $extraHeaders = [])
     {
         $headers = [
             'Content-Type'        => 'application/pdf',
             'Content-Disposition' => 'inline; filename="' . $filename . '"',
         ];
+
+        if (is_array($extraHeaders)) {
+            $headers = $headers + $extraHeaders;
+        }
 
         parent::__construct($content, 200, $headers);
     }
