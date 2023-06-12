1052ed14ffaa2a0b6fde0fc5d6a52a7453a49e1e
Michiel Vugteveen <info@it-can.nl>
2023-06-12 13:39:02 +0200

    Add CryptHelper for encrypting large files

---
+++ src/Helpers/CryptHelper.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace ITCAN\LaravelHelpers\Helpers;
+
+use Illuminate\Support\Facades\Crypt as CryptBase;
+use Illuminate\Support\Facades\File;
+
+class CryptHelper
+{
+    /**
+     * @param $baseFilePath
+     * @param $finalFilePath
+     * @param $deleteBaseFile
+     * @return mixed
+     */
+    public static function encryptLargeFile($baseFilePath, $finalFilePath, $deleteBaseFile = false)
+    {
+        $chunkSize = 1024 * 1024;
+
+        // Open the file for reading
+        $fileHandle = fopen($baseFilePath, 'r');
+
+        // Open a new file for writing the encrypted chunks
+        File::put($finalFilePath, '');
+
+        // Loop through the file in chunks and encrypt each chunk
+        while (! feof($fileHandle)) {
+            $chunk = fread($fileHandle, $chunkSize);
+
+            File::append($finalFilePath, self::encrypt($chunk));
+
+            unset($chunk);
+        }
+
+        // Close the file handles
+        fclose($fileHandle);
+
+        if ($deleteBaseFile) {
+            File::delete($baseFilePath);
+        }
+
+        // Return the filename of the encrypted file
+        return $finalFilePath;
+    }
+
+    /**
+     * @param $data
+     * @return string
+     */
+    private static function encrypt($data)
+    {
+        return CryptBase::encrypt($data);
+    }
+}
