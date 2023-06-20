33c1dbd1eb2e2aa150c9febbf7baebb49f30e22e
Michiel Vugteveen <info@it-can.nl>
2023-06-20 13:44:15 +0200

    Remove deprecated file encryption function

--- src/Helpers/CryptHelper.php
+++ src/Helpers/CryptHelper.php
@@ -61,42 +61,4 @@ class CryptHelper
     {
         return new FileEncrypter(config('app.file_encrypt.key'), config('app.file_encrypt.cipher'));
     }
-
-    /**
-     * @param  $baseFilePath
-     * @param  $finalFilePath
-     * @param  $deleteBaseFile
-     * @return mixed
-     *
-     * @deprecated
-     */
-    public static function encryptLargeFile($baseFilePath, $finalFilePath, $deleteBaseFile = false)
-    {
-        $chunkSize = 1024 * 1024;
-
-        // Open the file for reading
-        $fileHandle = fopen($baseFilePath, 'r');
-
-        // Open a new file for writing the encrypted chunks
-        File::put($finalFilePath, '');
-
-        // Loop through the file in chunks and encrypt each chunk
-        while (! feof($fileHandle)) {
-            $chunk = fread($fileHandle, $chunkSize);
-
-            File::append($finalFilePath, Crypt::encrypt($chunk));
-
-            unset($chunk);
-        }
-
-        // Close the file handles
-        fclose($fileHandle);
-
-        if ($deleteBaseFile) {
-            File::delete($baseFilePath);
-        }
-
-        // Return the filename of the encrypted file
-        return $finalFilePath;
-    }
 }
